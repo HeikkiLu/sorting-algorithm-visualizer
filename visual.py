f897848fe9f0cd6a606fd5b1733f063309c4c86c
HeikkiLu <53558226+HeikkiLu@users.noreply.github.com>
2020-01-12 15:43:44 +0200

    Added 'Select sorting method' button, so the user do not have to start the application again if they whish to visualize another method.

--- visual.py
+++ visual.py
@@ -4,7 +4,7 @@ import bubblesort, insertionsort, selectionsort
 
 BAR_SPACING, BAR_WIDTH, EDGE_OFFSET = 11, 10, 3
 DATA_SIZE = GRAPH_SIZE = (800,500) 
-
+sg.ChangeLookAndFeel('BrownBlue') 
 def draw_bars(graph, items):
     for i, item in enumerate(items):
         graph.DrawRectangle(top_left=(i * BAR_SPACING + EDGE_OFFSET, item),
@@ -12,51 +12,54 @@ def draw_bars(graph, items):
 
 
 def main():
-    sg.theme('DarkBlue1')
     # List to sort
     num_bars = DATA_SIZE[0] // (BAR_WIDTH+1)
     arr = [DATA_SIZE[1]//num_bars*i for i in range(1, num_bars)]
-    random.shuffle(arr)
 
     # Window layout
     graph = sg.Graph(GRAPH_SIZE, (0,0), DATA_SIZE)
-    layout = [[graph]]
+    layout = [[graph], [sg.Button('Select sorting method')]]
+            
 
     window = sg.Window('Algorithm Visualizer', layout)
     window.Finalize()
 
     draw_bars(graph, arr)
     
-    l2 = [[sg.T('Choose sorting method')],
-          [sg.Listbox(['Bubble', 'Insert', 'Selection'], size=(12,3))],
-          [sg.Ok()],]
-    w2 = sg.Window('Choose sorting method', l2)
-    button, values = w2()
-    w2.close()
-
-    if values[0][0] == 'Bubble':
-        sort = bubblesort.bubbleSort(arr)
-    elif values[0][0] == 'Insertion':
-        sort = insertionsort.insertionSort(arr)
-    else:
-        sort = selectionsort.selectionSort(arr)
-
-    timeout=10
-
-    for partially_sorted_list in sort:
-        event, values = window.read(timeout=timeout)
-        if event is None:
-            break
-        graph.Erase()
-        draw_bars(graph, partially_sorted_list)
-        timeout = int(10)
-    sg.Popup('Sorting done.')
-
     while True:
         event, values = window.read()
         if event in (None, 'Exit'):
             break
+        if event == 'Select sorting method':
+            graph.Erase()
+            random.shuffle(arr)
+            draw_bars(graph, arr)
+            l2 = [[sg.T('Choose sorting method')],
+            [sg.Listbox(['Bubble', 'Insert', 'Selection'], size=(12,3))],
+            [sg.Ok()],]
+            w2 = sg.Window('Choose sorting method', l2)
+            button, values = w2()
+            w2.close()
+            
+            if values[0][0] == 'Bubble':
+                sort = bubblesort.bubbleSort(arr)
+            elif values[0][0] == 'Insertion':
+                sort = insertionsort.insertionSort(arr)
+            else:
+                sort = selectionsort.selectionSort(arr)
+            
+            timeout=10
+
+            for partially_sorted_list in sort:
+                event, values = window.read(timeout=timeout)
+                if event is None:
+                    break
+                graph.Erase()
+                draw_bars(graph, partially_sorted_list)
+                timeout = int(10)
+            sg.Popup('Sorting done.')
+
+
 
 if __name__ == "__main__":
     main()
-
